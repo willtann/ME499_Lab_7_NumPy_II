a2c1b1e8fb4f12eb8662e53ba57f51be4aeba59a
Tanner WIlliams <97181243+willtann@users.noreply.github.com>
2022-03-02 13:06:57 -0800

    Wednesday 03.02.2022

--- eggholder.py
+++ eggholder.py
@@ -21,12 +21,15 @@ def eggholder(x, y):
 
 
 def minimize_eggholder(guess, max_calls=100):
-    minimized = [None] * len(guess)
+    # function to optimize
     egg = lambda xy: eggholder(xy[0], xy[1])
     out = []
     for g, my_xy in enumerate(guess):
+        # optimize function with vaious guesses given by input
         mini = optimize.fmin(func=egg, x0=my_xy, maxfun=max_calls, full_output=True)
+        # save results
         out.append((tuple(mini[0]), mini[1]))
+    # return 2 values... ((x, y), function_value)
     return np.array(out, dtype=object)
 
 
@@ -35,11 +38,13 @@ if __name__ == '__main__':
 
     # fmin
     my_test = minimize_eggholder(guess)
+    # function values from optimized function
     f_min = my_test[:, 1]
     # print('fmin: ', f_min)
 
     # gmin
     g_min = [None] * len(guess)
+    # fucntion values from global
     for i, loc in enumerate(guess):
         global_min = eggholder(loc[0], loc[1])
         g_min[i] = global_min
@@ -57,36 +62,3 @@ if __name__ == '__main__':
     plt.xlabel('Absolute difference (fmin - global min)')
     plt.savefig('Histogram')
     plt.show()
-
-    # # x and y from fmin
-    # print(my_test)
-    # fmin_x = []
-    # fmin_y = []
-    # for y, this in enumerate(f_min):
-    #     fmin_x.append(my_test[y][0][0])
-    #     fmin_y.append(my_test[y][0][1])
-    #
-    # print('fmin_x')
-    # print(fmin_x)
-    # print('fmin_y')
-    # print(fmin_y)
-    #
-    #
-    # # x and y from gmin
-    # gmin_x = guess[:, 0]
-    # gmin_y = guess[:, 1]
-    # print('gmin_x')
-    # print(gmin_x)
-    # print('gmin_y')
-    # print(gmin_y)
-    #
-    # # Calculating differences
-    # diff_x = abs(gmin_x - fmin_x)
-    # print('diff_x')
-    # print(diff_x)
-    # diff_y = abs(gmin_y - fmin_y)
-    # print('diff_y')
-    # print(diff_y)
-    # diff = diff_x + diff_y
-    # print('diff')
-    # print(diff)
