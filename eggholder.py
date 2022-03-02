957cdd45585d5846123cc7b6ae3c746eeaf0d11e
Tanner WIlliams <97181243+willtann@users.noreply.github.com>
2022-03-02 13:14:07 -0800

    Wednesday 03.02.2022

--- eggholder.py
+++ eggholder.py
@@ -30,35 +30,35 @@ def minimize_eggholder(guess, max_calls=100):
         # save results
         out.append((tuple(mini[0]), mini[1]))
     # return 2 values... ((x, y), function_value)
-        return np.array(out, dtype=object)
+    return np.array(out, dtype=object)
 
-
-if __name__ == '__main__':
-    guess = np.random.randint(-512, 512, size=(1000, 2))
-
-    # fmin
-    my_test = minimize_eggholder(guess)
-    # function values from optimized function
-    f_min = my_test[:, 1]
-    # print('fmin: ', f_min)
-
-    # gmin
-    g_min = [None] * len(guess)
-    # fucntion values from global
-    for i, loc in enumerate(guess):
-        global_min = eggholder(loc[0], loc[1])
-        g_min[i] = global_min
-    np.array(g_min).reshape((len(guess)), 1)
-    # print('gmin:', g_min)
-
-    # diff
-    diff = abs(f_min - g_min)
-    # print(diff)
-
-    # Plotting
-    plt.hist(diff, bins=25)
-    plt.title('Histogram: Absolute difference')
-    plt.ylabel('Frequency')
-    plt.xlabel('Absolute difference (fmin - global min)')
-    plt.savefig('Histogram')
-    plt.show()
+#
+# if __name__ == '__main__':
+#     guess = np.random.randint(-512, 512, size=(1000, 2))
+#
+#     # fmin
+#     my_test = minimize_eggholder(guess)
+#     # function values from optimized function
+#     f_min = my_test[:, 1]
+#     # print('fmin: ', f_min)
+#
+#     # gmin
+#     g_min = [None] * len(guess)
+#     # fucntion values from global
+#     for i, loc in enumerate(guess):
+#         global_min = eggholder(loc[0], loc[1])
+#         g_min[i] = global_min
+#     np.array(g_min).reshape((len(guess)), 1)
+#     # print('gmin:', g_min)
+#
+#     # diff
+#     diff = abs(f_min - g_min)
+#     # print(diff)
+#
+#     # Plotting
+#     plt.hist(diff, bins=25)
+#     plt.title('Histogram: Absolute difference')
+#     plt.ylabel('Frequency')
+#     plt.xlabel('Absolute difference (fmin - global min)')
+#     plt.savefig('Histogram')
+#     plt.show()
