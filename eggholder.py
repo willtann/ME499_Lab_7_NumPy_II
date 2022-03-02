8d77a1bd842f3b5de6fcb2a20476154717a4b050
Tanner WIlliams <97181243+willtann@users.noreply.github.com>
2022-03-02 12:57:52 -0800

    Wednesday 03.02.2022

--- eggholder.py
+++ eggholder.py
@@ -36,21 +36,57 @@ if __name__ == '__main__':
     # fmin
     my_test = minimize_eggholder(guess)
     f_min = my_test[:, 1]
+    # print('fmin: ', f_min)
+
     # gmin
     g_min = [None] * len(guess)
     for i, loc in enumerate(guess):
         global_min = eggholder(loc[0], loc[1])
-        # g_min.append(global_min)
         g_min[i] = global_min
     np.array(g_min).reshape((len(guess)), 1)
+    # print('gmin:', g_min)
 
-    diff = np.abs(g_min, f_min)
+    # diff
+    diff = abs(f_min - g_min)
+    # print(diff)
+
     # Plotting
     plt.hist(diff, bins=25)
     plt.title('Histogram: Absolute difference')
     plt.ylabel('Frequency')
     plt.xlabel('Absolute difference (fmin - global min)')
+    plt.savefig('Histogram')
     plt.show()
 
-
-
+    # # x and y from fmin
+    # print(my_test)
+    # fmin_x = []
+    # fmin_y = []
+    # for y, this in enumerate(f_min):
+    #     fmin_x.append(my_test[y][0][0])
+    #     fmin_y.append(my_test[y][0][1])
+    #
+    # print('fmin_x')
+    # print(fmin_x)
+    # print('fmin_y')
+    # print(fmin_y)
+    #
+    #
+    # # x and y from gmin
+    # gmin_x = guess[:, 0]
+    # gmin_y = guess[:, 1]
+    # print('gmin_x')
+    # print(gmin_x)
+    # print('gmin_y')
+    # print(gmin_y)
+    #
+    # # Calculating differences
+    # diff_x = abs(gmin_x - fmin_x)
+    # print('diff_x')
+    # print(diff_x)
+    # diff_y = abs(gmin_y - fmin_y)
+    # print('diff_y')
+    # print(diff_y)
+    # diff = diff_x + diff_y
+    # print('diff')
+    # print(diff)
